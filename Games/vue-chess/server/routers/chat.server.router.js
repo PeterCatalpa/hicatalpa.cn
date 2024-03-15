164c99cd89b8a046f48f5fa60036f59e88066482
PeterCatalpa <petercatalpa@outlook.com>
2024-03-15 19:26:17 +0800

    Site updated: 2024-03-15 19:26:15

--- Games/vue-chess/server/routers/chat.server.router.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app, express, controllers) {
-  var chat = express.Router();
-    chat.route('/upload')
-            .post(controllers.chat.uploadFile)
-            .put(controllers.chat.deleteImage)
-    app.use('/chat', chat);
-};

c6533565a3ac8158baa36a061ea7713fa736698b
petercatalpa <petercatalpa@outlook.com>
2025-08-30 10:04:03 +0800

    111

--- src/config.ts
+++ src/config.ts
@@ -9,11 +9,11 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "HiCatalpa.cn",
-	subtitle: "Welcome to my website!",
+	subtitle: "梓树の小站",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: true, // Hide the theme color picker for visitors
+		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: false,
